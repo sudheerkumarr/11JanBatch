c493b404aeed12f138d562c6107eb836b52d214a
Sudheer Kumar R <rskumar780@gmail.com>
2023-01-12 15:31:32 +0530

    Delete JavaWorkspace/Basics/src/day4 directory
    removed

--- JavaWorkspace/Basics/src/day4/Child.java
+++
@@ -1,9 +0,0 @@
-package day4;
-
-public class Child extends Parent{
-	int z = 1000;
-	
-	public int m3() {
-		return z;
-	}
-}
--- JavaWorkspace/Basics/src/day4/GrandParent.java
+++
@@ -1,9 +0,0 @@
-package day4;
-
-public class GrandParent {
-	int x =10;
-	
-	public int m1() {
-		return x;
-	}
-}
